a663aecd93d11a60cb503e7ff28085cbcae35901
Le Trung Dung <ekkophantoms123@gmail.com>
2023-08-18 08:04:20 +0700

    ENV Variables with config/mongoose/ejs library

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController],
+  imports: [
+    // MongooseModule.forRoot('mongodb+srv://dungbum:1GVSRUn6j6U5siGU@cluster0.fhbp7dw.mongodb.net/'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot(
+      { isGlobal: true }
+    )],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
