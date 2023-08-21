a1439c032a8242bb54b5dd49b365925d1e0e7505
Le Trung Dung <ekkophantoms123@gmail.com>
2023-08-21 14:45:56 +0700

    Add ms form

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,6 +7,7 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './passport/jwt.strategy';
+import ms from 'ms'
 
 @Module({
   imports: [UsersModule, PassportModule,
@@ -14,9 +15,9 @@ import { JwtStrategy } from './passport/jwt.strategy';
       imports: [ConfigModule],
       //config in .env
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
+        secret: configService.get<string>('JWT_ACCESS_TOKEN'),
         signOptions: {
-          expiresIn: configService.get<string>('JWT_EXPIRE'),
+          expiresIn: ms(configService.get<string>('JWT_ACCESS_EXPIRE')),
         },
       }),
       inject: [ConfigService],
