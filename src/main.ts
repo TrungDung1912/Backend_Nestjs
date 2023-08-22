75856916b67e7d0064c44c189345a572292ff37d
Le Trung Dung <ekkophantoms123@gmail.com>
2023-08-22 13:55:38 +0700

    CORS config

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,14 @@ async function bootstrap() {
   app.setViewEngine('ejs');
 
   app.useGlobalPipes(new ValidationPipe());
+  //config CORS
+  app.enableCors(
+    {
+      "origin": "*",
+      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+      "preflightContinue": false,
+    }
+  );
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
