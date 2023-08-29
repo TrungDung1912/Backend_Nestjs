2e59d7be761a564426c0e0d8bfa28bbb187ffa36
Le Trung Dung <ekkophantoms123@gmail.com>
2023-08-29 15:16:51 +0700

    Refresh Token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,15 @@ import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { IUser } from 'src/users/users.interface';
 import { RegisterUserDto } from 'src/users/dto/create-user.dto';
+import { ConfigService } from '@nestjs/config';
+import ms from 'ms';
 
 @Injectable()
 export class AuthService {
     constructor(
         private usersService: UsersService,
         private jwtService: JwtService,
+        private configService: ConfigService
     ) { }
 
     async validateUser(username: string, pass: string): Promise<any> {
@@ -32,8 +35,11 @@ export class AuthService {
             email,
             role
         };
+
+        const refresh_token = this.createRefreshToken(payload)
         return {
             access_token: this.jwtService.sign(payload),
+            refresh_token,
             user: {
                 _id,
                 name,
@@ -51,8 +57,11 @@ export class AuthService {
         }
     }
 
-    createRefreshToken = (payload) => {
-        const refreshToken = this.jwtService.sign(payload)
+    createRefreshToken = (payload: any) => {
+        const refreshToken = this.jwtService.sign(payload, {
+            secret: this.configService.get<string>("JWT_REFRESH_TOKEN_SECRET"),
+            expiresIn: ms(this.configService.get<string>('JWT_REFRESH_EXPIRE')) / 1000
+        })
         return refreshToken
     }
 }
